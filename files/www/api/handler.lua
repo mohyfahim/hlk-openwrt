1ee4a0b0291cd5a7decb07f5e93d08d92fc852b6
mohyfahim <fahimohy@gmail.com>
2025-02-24 22:33:52 +0330

    update: add lua api to check the process

--- files/www/api/handler.lua
+++ files/www/api/handler.lua
@@ -1,10 +1,11 @@
 local io = require "io"
 
-
 function get_mac_from_dhcp(ip)
     local dhcp_file = io.open("/tmp/dhcp.leases", "r")
-    if not dhcp_file then return nil end
-    
+    if not dhcp_file then
+        return nil
+    end
+
     for line in dhcp_file:lines() do
         local timestamp, mac, client_ip = line:match("(%d+) (%S+) (%S+)")
         if client_ip == ip then
@@ -16,25 +17,50 @@ function get_mac_from_dhcp(ip)
     return nil
 end
 
+function get_update_pid()
+    -- Use ps + grep to find the process (avoids matching the grep command itself)
+    local cmd = "pgrep pdate.sh'"
+    local handle = io.popen(cmd)
+    local output = handle:read("*a")
+    handle:close()
+
+    -- Parse the PID from the first matching line
+    for line in output:gmatch("[^\r\n]+") do
+        local pid = line:match("%s*(%d+)") -- Extract the first number (PID)
+        if pid then
+            return tonumber(pid)
+        end
+    end
+    return nil -- Return nil if no process found
+end
 
 function handle_request(env)
-    	if env.REQUEST_METHOD == "GET" and env.PATH_INFO == "/mac" then
-		uhttpd.send("Status: 200 OK\r\n")                              
-        	uhttpd.send("Content-Type: application/json\r\n\r\n")
-        local query = env.QUERY_STRING or ""                 
-        local ip = query:match("ip=([^&]+)") or ""           
-                                                             
-        local mac = get_mac_from_dhcp(ip)                    
-                                                  
-        if mac ~= nil then                        
+    if env.REQUEST_METHOD == "GET" and env.PATH_INFO == "/mac" then
+        uhttpd.send("Status: 200 OK\r\n")
+        uhttpd.send("Content-Type: application/json\r\n\r\n")
+        local query = env.QUERY_STRING or ""
+        local ip = query:match("ip=([^&]+)") or ""
+
+        local mac = get_mac_from_dhcp(ip)
+
+        if mac ~= nil then
             local resp = string.format("{\"mac\":\"%s\"}", mac)
-            uhttpd.send(resp)                                  
-        else                                                   
-           uhttpd.send("{\"mac\":null}")                       
-        end                       
-	else
-		uhttpd.send("Status: 404 Not Found\r\n")
-                uhttpd.send("Content-Type: application/json\r\n\r\n") 
-		uhttpd.send("{\"message\":\"not found\"}")                       
-	end
+            uhttpd.send(resp)
+        else
+            uhttpd.send("{\"mac\":null}")
+        end
+    elseif env.REQUEST_METHOD == "GET" and env.PATH_INFO == "/status" then
+        uhttpd.send("Status: 200 OK\r\n")
+        uhttpd.send("Content-Type: application/json\r\n\r\n")
+        local pid = get_update_pid()
+        if pid then
+            uhttpd.send("{\"status\": \"running\"}")
+        else
+            uhttpd.send("{\"status\": \"stop\"}")
+        end
+    else
+        uhttpd.send("Status: 404 Not Found\r\n")
+        uhttpd.send("Content-Type: application/json\r\n\r\n")
+        uhttpd.send("{\"message\":\"not found\"}")
+    end
 end
